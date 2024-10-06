00bfd0d68d330645b4b7d11916d8311ee88ed08c
Gabriela Aguiar <gabrielaaguiarmarquez@gmail.com>
2024-10-06 16:43:51 -0500

    NavBar responsive.

---
+++ scripts/index.js
@@ -0,0 +1,47 @@
+    const acordeonPreguntas = document.querySelectorAll('.body__acordeon-pregunta');
+    const acordeonRespuestas = document.querySelectorAll('.body__acordeon-respuesta');
+
+    const cerrarTodasLasRespuestas = () => {
+        acordeonPreguntas.forEach((pregunta) => pregunta.style.display = 'flex');
+        acordeonRespuestas.forEach((respuesta) => respuesta.style.display = 'none');
+    }
+
+    acordeonPreguntas.forEach((pregunta) => {
+        const abrirAcordeon = pregunta.querySelector('.fa-caret-down');
+        abrirAcordeon.addEventListener('click', () => {
+            cerrarTodasLasRespuestas();
+            pregunta.style.display = 'none';
+            pregunta.nextElementSibling.style.display = 'flex';
+        });
+    });
+
+    acordeonRespuestas.forEach((respuesta) => {
+        const cerrarAcordeon = respuesta.querySelector('.fa-caret-up');
+        cerrarAcordeon.addEventListener('click', () => {
+            respuesta.style.display = 'none';
+            respuesta.previousElementSibling.style.display = 'flex';
+        });
+    });
+
+// Menú Hamburguesa
+const menu = document.querySelector('.header__nav-menu-cont');
+const menuCerrado = document.querySelectorAll('.header__nav-mobile-hidden');
+const menuAbierto = document.querySelector('.oculto');
+const headerNav = document.querySelector('.header__nav');
+const equis = document.querySelector('.oculto__menu-cerrado');
+
+menu.addEventListener('click', () => {
+  menuCerrado.forEach(clase => {
+    clase.style.display = 'none';
+  });
+  menuAbierto.style.display = 'flex'; 
+  headerNav.style.height = '100dvh';
+});
+
+equis.addEventListener('click', () => {
+  menuCerrado.forEach(clase => {
+    clase.style.display = 'flex';
+  });
+  menuAbierto.style.display = 'none';
+  headerNav.style.height = '10vh';
+});
